b7ab4ce331982c861cf45eb25bab26ae12fc1930
Konulb13 <143033792+Konulb13@users.noreply.github.com>
2024-05-14 22:26:34 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,10 @@
 package org.example;
 import java.sql.*;
 public class Main {
-
-//    private static final String URL = "jdbc:mysql://localhost:3306/apartments";
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "3713k";
-
     public static void main(String[] args) {
         selectFlats();
     }
     public static void selectFlats() {
-//        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD); PreparedStatement preparedStatement = connection.prepareStatement("select * from flat"); ResultSet resultSet = preparedStatement.executeQuery();) {
-//            ResultSetMetaData rsmd = resultSet.getMetaData();
-//            while (resultSet.next()) {
-//                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-//                    System.out.print(resultSet.getString(i) + "\t");
-//                }
-//                System.out.println();
-//            }
-//            resultSet.close();
-//            preparedStatement.close();
-//            if (!connection.isClosed()) {
-//                System.out.println("Connection is closed");
-//            }
-//        } catch (SQLException e) {
-//            System.out.println("there was a problem connecting to the database");
-//            e.printStackTrace();
-//        }
         DBConnect db = new DBConnect();
         String query = "select * from flat";
         try {
